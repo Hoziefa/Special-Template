84758b8e41453984a4af13d225767addb87628a0
Hoziefa Alhassan <hoziefaalhasan@gmail.com>
2021-03-20 03:46:44 +0200

    Remove Comments

--- src/types/typeAliases.ts
+++ src/types/typeAliases.ts
@@ -2,4 +2,4 @@ export type Callback<T = any> = (...args: T[]) => void;
 
 export type PartialRequired<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
 
-export type HTMLElementEvent<T extends Element, C = T> = Event & { target: T; currentTarget: C }; //>: Defaulting another generic type with previous generic type
+export type HTMLElementEvent<T extends Element, C = T> = Event & { target: T; currentTarget: C };
